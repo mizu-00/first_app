e51a31713fd7484b6988f73a937e63233ff46692
mizu-00 <minramu1111@gmail.com>
2020-08-19 20:50:27 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
   end
 end
