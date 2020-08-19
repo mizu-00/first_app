814802df7771de750f4aaf5e54bddc4831dc066b
mizu-00 <minramu1111@gmail.com>
2020-08-19 20:51:35 +0900

    Revert "Update posts_controller.rb"
    This reverts commit e51a31713fd7484b6988f73a937e63233ff46692.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
+    @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
   end
 end
